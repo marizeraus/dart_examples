7ba87859b75321f7784c6de55470a9f48501b7c8
Mariana Suarez de Oliveira <38335557+marizeraus@users.noreply.github.com>
2021-10-27 21:12:38 -0300

    Add files via upload

---
+++ circle.dart
@@ -0,0 +1,16 @@
+
+import 'dart:math' as math;
+
+class Circle {
+  double radius;
+
+  Circle(this.radius);
+
+  double get area => math.pi * math.pow(radius, 2);
+}
+
+void main() {
+
+  print(Circle(2.0).area); 
+  print(Circle(2).area);
+}
